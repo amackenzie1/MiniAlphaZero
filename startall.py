0e94eacd525cfe7a1ae8c0e4a76f91031e77efa9
amackenzie1 <andrushamackenzie@yahoo.ca>
2021-05-26 17:17:28 -0400

    fixed more idiocy

--- startall.py
+++ startall.py
@@ -7,7 +7,7 @@ num_computers = int(sys.argv[3])
 
 command = ""
 
-for i in range(num_processes//num_episodes):
+for i in range(num_processes//num_computers):
     command += f"python mcts.py {num_processes} {num_episodes} & "
 command += "ls"
 
